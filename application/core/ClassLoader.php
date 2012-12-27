d277312275a2e01f958e699ae5f70ddc48f20164
otsukano <mopinfish@gmail.com>
2012-12-27 19:44:26 +0900

    [simply] bootstrapおよびフロントコントローラ作成

--- application/core/ClassLoader.php
+++ application/core/ClassLoader.php
@@ -5,15 +5,23 @@ class ClassLoader
 
     public function register()
     {
-        spl_auto_load_register(array($this, 'loadClass'));
+        spl_autoload_register(array($this, 'loadClass'));
     }
 
     public function registerDir($dir)
     {
+        $this->dirs[] = $dir;
     }
 
     public function loadClass($class)
     {
+        foreach ($this->dirs as $dir) {
+            $file = $dir . '/' . $class . '.php';
+            if (is_readable($file)) {
+                require $file;
+
+                return;
+            }
+        }
     }
 }
-echo 'hogehoge';
