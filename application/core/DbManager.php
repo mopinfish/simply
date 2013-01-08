e976c0e300e6bd7b60e3c598d6af607fee0c1ddb
otsukano <mopinfish@gmail.com>
2013-01-09 02:31:41 +0900

    [Simply] Router, Response, DbManager, DbRepository追加

---
+++ application/core/DbManager.php
@@ -0,0 +1,79 @@
+<?php
+
+class DbManager
+{
+    protected $connections = array();
+
+    protected $repositoryConnectionMap = array();
+
+    protected $repositories = array();
+
+    public function connect($name, $params)
+    {
+        $params = array_merge(array(
+            'dsn' => null,
+            'user' => '',
+            'password' => '',
+            'options' => array()
+        ), $params);
+
+        $con = new PDO(
+            $params['dsn'],
+            $params['user'],
+            $params['password'],
+            $params['options']
+        );
+
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $this->connections[$name] = $con;
+    }
+
+    public function getConnection($name = null)
+    {
+        if (is_null($name)) {
+            return current($this->connections);
+        }
+        return $this->connections[$name];
+    }
+
+    public function setRepositoryConnectionMap($repositoryName, $name)
+    {
+        $this->repositoryConnectionMap[$repositoryName] = $name;
+    }
+
+    public function getConnectionForRepository($repositoryName)
+    {
+        if (isset($this->repositoryConnectionMap[$repositoryName])) {
+            $name = $this->repositoryConnectionMap[$repositoryName];
+            $con = $this->getConnection($name);
+        } else {
+            $con = $this->getConnection();
+        }
+        return $con;
+    }
+
+    public function get($repositoryName)
+    {
+        if (!isset($this->repositories[$repositoryName])) {
+            $repositoryClass = $repositoryName . 'Repository';
+            $con = $this->getConnectionForRepository($repositoryName);
+
+            $repository = new $repositoryClass($con);
+
+            $this->repositories[$repositoryName] = $repository;
+        }
+        return $this->repositories[$repositoryName];
+    }
+
+    public function __destruct()
+    {
+        foreach ($this->repositories as $repository) {
+            unset($repository);
+        }
+
+        foreach ($this->connections as $con) {
+            unset($con);
+        }
+    }
+}
--- application/core/Router.php
+++ application/core/Router.php
@@ -8,4 +8,39 @@ class Router
     {
         $this->routes = $this->compileRoutes($definitions);
     }
+
+    public function compileRoutes($definitions)
+    {
+        $routes = array();
+
+        foreach ($definitions as $url => $params) {
+            $tokens = explode('/', ltrim($url, '/'));
+            foreach ($tokens as $i => $token) {
+                if (0 === strpos($token, ':')) {
+                    $name = substr($token, 1);
+                    $token = '(?P<' . $name . '>[^/]+)';
+                }
+                $tokens[$i] = $token;
+            }
+
+            $pattern = '/' . implode('/', $tokens);
+            $routes[$pattern] = $params;
+        }
+        return $routes;
+    }
+
+    public function resolve($path_info)
+    {
+        if ('/' !== substr($path_info, 0, 1)) {
+            $path_info = '/' . $path_info;
+        }
+
+        foreach ($this->routes as $pattern => $params) {
+            if (preg_match('#^' . $patternn . '$#', $path_info, $matches)) {
+                $params = array_merge($params, $matches);
+                return $params;
+            }
+        }
+        return false;
+    }
 }
