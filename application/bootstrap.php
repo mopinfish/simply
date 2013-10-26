2f703f9cdedcf61402b8f440b500bdb452263ee8
otsukano <otsukanoboru@next-group.jp>
2013-10-26 23:09:23 +0900

    [simply]パスを修正

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 
-if (file_exists($loaderFile = dirname(__FILE__) . 'core/ClassLoader.php')) {
+if (file_exists($loaderFile = dirname(__FILE__) . '/core/ClassLoader.php')) {
     require_once $loaderFile;
     $loader = new ClassLoader();
     $loader->registerDir(dirname(__FILE__) . '/core');
     $loader->registerDir(dirname(__FILE__) . '/models');
     $loader->register();
 }
-if (file_exists($twigLoaderFile = dirname(__FILE__) . '../vender/Twig/Autoloader.php')) {
+if (file_exists($twigLoaderFile = dirname(__FILE__) . '/../vendor/Twig/Autoloader.php')) {
     require_once $twigLoaderFIle;
     Twig_Autoloader::register();
 }
